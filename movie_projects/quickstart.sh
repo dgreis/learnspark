b0ad706f309d9565630b4a356b5b111ae06f33be
David Greis <dgreis@ischool.berkeley.edu>
2017-02-21 09:56:23 -0500

    open up other ports for other pyspark shells that may spawn

--- movie_projects/quickstart.sh
+++ movie_projects/quickstart.sh
@@ -1,5 +1,5 @@
 docker rm master
-docker run -i -p 47223:22 -p 5050:5050 -p 4040:4040 -p 48080:8080 -p 47777:7777 --name master dgreis/movie:latest bin/bash -c "bash bootstrap.sh -d -master 2" &
+docker run -i -p 47223:22 -p 5050:5050 -p 4040:4040 -p 4041:4041 -p 4042:4042 -p 48080:8080 -p 47777:7777 --name master dgreis/movie:latest bin/bash -c "bash bootstrap.sh -d -master 2" &
 
 docker rm slave2
 docker run -i -p 47224:22 -p 8081:8081 --name slave2 dgreis/movie:latest bin/bash -c "bash bootstrap.sh -d -slave 2" &
